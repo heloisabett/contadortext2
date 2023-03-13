feedb954e06add7b53c6cb60071e48e001fbd8b5
heloisabett <heloisabettramosifc@gmail.com>
2023-03-13 15:11:25 -0300

    arrumar

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+import Contador from './src/components/Contador'
+
+export default function App() {
+  return (
+    <View>
+      <Contador />
+    </View>
+  )
+}
+
